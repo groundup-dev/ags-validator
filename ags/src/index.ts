782513a44849723b698c9441b599c1289a6d97bc
James Holcombe <jholcombe@hotmail.co.uk>
2024-09-30 19:56:16 +0100

    add ags changes

--- ags/src/index.ts
+++ ags/src/index.ts
@@ -1,4 +1,12 @@
 import { validateAgsData } from "./validate";
 import type { AgsError, AgsRaw, AgsDictionaryVersion, GroupRaw } from "./types";
+import { parsedAgsToString } from "./parse";
 
-export { validateAgsData, AgsError, AgsRaw, AgsDictionaryVersion, GroupRaw };
+export {
+  validateAgsData,
+  AgsError,
+  AgsRaw,
+  AgsDictionaryVersion,
+  GroupRaw,
+  parsedAgsToString,
+};
