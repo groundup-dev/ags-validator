de42559ff07d5d24ab4f5c324ccec2f897dca64d
James Holcombe <78622696+jamesholcombe@users.noreply.github.com>
2024-10-25 15:40:41 +0100

    add basic metric tracking (#24)
    * add basic metric tracking
    
    * remove click register

--- ags-validator-app/components/RegisterForm/RegisterForm.tsx
+++ ags-validator-app/components/RegisterForm/RegisterForm.tsx
@@ -16,6 +16,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Check, LoaderCircle } from "lucide-react";
 import { useState } from "react";
+import { track } from "@vercel/analytics";
 
 const formSchema = z.object({
   firstName: z.string().min(1, "First name is required"),
@@ -50,9 +51,11 @@ export function RegisterForm() {
       console.error("Failed to submit form");
       const data = await response.json();
       setError(data.error);
+      track("failed-registration", { error: data.error });
 
       return;
     } else {
+      track("completed-registration");
       setIsSuccess(true);
     }
   }
