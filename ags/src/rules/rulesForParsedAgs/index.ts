00111e8d18144bf48eb8e37c456e9f24ea95ee47
James Holcombe <78622696+jamesholcombe@users.noreply.github.com>
2024-10-28 12:58:37 +0000

    add missing validation steps (#35)

--- ags/src/rules/rulesForParsedAgs/index.ts
+++ ags/src/rules/rulesForParsedAgs/index.ts
@@ -1,5 +1,13 @@
 import { rule19, rule19a, rule19b, rule7 } from "./checkGroupAndHeadings";
 import { rule10a, rule10b, rule10c, rule9 } from "./checkHeadingsWithDict";
+import {
+  rule11,
+  rule13,
+  rule14,
+  rule15,
+  rule16,
+  rule17,
+} from "./checkRequiredGroups";
 import { rule8 } from "./checkDataTypes";
 
 export const rulesForParsedAgs = {
@@ -15,4 +23,10 @@ export const rulesForParsedAgsWithDict = {
   rule10b,
   rule10c,
   rule9,
+  rule11,
+  rule13,
+  rule14,
+  rule15,
+  rule16,
+  rule17,
 };
