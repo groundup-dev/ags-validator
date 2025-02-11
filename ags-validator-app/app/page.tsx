bea0b6a15aee735571b5688666c988f67be7f0eb
Ed Fisk <edward.fisk@outlook.com>
2025-02-11 22:02:31 +0000

    Refactor top section

--- ags-validator-app/app/page.tsx
+++ ags-validator-app/app/page.tsx
@@ -25,46 +25,17 @@ import { Logo } from "@/components/logo";
 
 export default function Page() {
   return (
-    <div className="overflow-hidden ">
-      <section
-        id="cta"
-        className="border-y bg-muted/50 py-12  px-10 w-full flex flex-col items-center"
-      >
-        <div className="px-5 container lg:grid lg:grid-cols-2 place-items-center">
-          <div className="lg:col-start-1">
-            <h2 className="text-2xl md:text-3xl font-bold">
-              Register for{" "}
-              <span className="bg-gradient-to-b from-primary/60 dark:bg-foreground/80 to-primary dark:to-primary text-transparent bg-clip-text">
-                early access
-              </span>
-            </h2>
-            <p className="text-muted-foreground text-lg mt-4 mb-8 lg:mb-0">
-              We&apos;re currently in closed beta, so join the waitlist to get
-              early access, and help shape GroundUp.
-            </p>
-          </div>
-          <div className="lg:col-start-2 lg:mt-0 mt-12">
-            <Button asChild size="lg">
-              <Link
-                href="https://groundup.cloud"
-                className="flex items-center gap-2"
-              >
-                <ExternalLink className="w-4 h-4" /> Explore GroundUp
-              </Link>
-            </Button>
-          </div>
-        </div>
-      </section>
-      <div className="flex flex-col  my-12 w-full mx-auto gap-12 px-2 ">
-        <div className="w-full ">
-          <div className="flex my-8 flex-wrap gap-4 items-center justify-center">
+    <div className="overflow-hidden">
+      <div className="flex flex-col lg:flex-row my-12 justify-between w-full mx-auto container gap-x-12 gap-y-2 px-10">
+        <div className="basis-1/2">
+          <div className="flex my-8 flex-wrap gap-4 items-center">
             <h1 className="text-3xl font-bold flex-0">AGS Editor</h1>
-            <div className="flex items-center gap-2 ">
+            <div className="flex items-center gap-2">
               <p className="text-lg">by</p>
               <Logo size="sm" />
             </div>
           </div>
-          <div className="w-full max-w-2xl mx-auto">
+          <div className="w-full max-w-2xl">
             <p className="text-lg mb-4">
               Edit and check your AGS data with ease.
             </p>
@@ -94,7 +65,7 @@ export default function Page() {
             </p>
           </div>
 
-          <div className="flex mb-8 justify-center ">
+          <div className="flex mb-8 justify-center">
             <div className="flex flex-col gap-8 w-full max-w-2xl">
               <div className="flex items-center gap-4 w-full">
                 <div className="flex-grow flex flex-col lg:flex-row min-h-24 lg:items-center border rounded-lg h-full bg-accent hover:scale-101 hover:shadow-sm transition-all">
@@ -154,6 +125,30 @@ export default function Page() {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+        <div className="basis-1/2">
+          <div className="border bg-muted/50 rounded-lg p-10 space-y-8 max-w-2xl my-12 mx-auto">
+            <div className="space-y-4">
+              <h2 className="text-2xl md:text-3xl font-bold">
+                Register for{" "}
+                <span className="bg-gradient-to-b from-primary/60 dark:bg-foreground/80 to-primary dark:to-primary text-transparent bg-clip-text">
+                  early access
+                </span>
+              </h2>
+              <p className="text-muted-foreground text-lg">
+                We&apos;re currently in closed beta, so join the waitlist to get
+                early access, and help shape GroundUp.
+              </p>
+            </div>
+            <Button asChild size="lg">
+              <Link
+                href="https://groundup.cloud"
+                className="flex items-center gap-2"
+              >
+                <ExternalLink className="w-4 h-4" /> Explore GroundUp
+              </Link>
+            </Button>
           </div>
         </div>
       </div>
